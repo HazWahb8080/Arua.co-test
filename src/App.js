b776739bc48ba3df0fd0471f49591d0e1cde10a0
HazWahb8080 <hazem.wahb100@gmail.com>
2022-07-28 02:03:42 +0200

    from tailwind css to scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ResultItem from "./components/resultItem/ResultItem";
 
@@ -18,28 +18,24 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         SetSearchResults(data.articles);
-        if(data.articles.length === 0) setNoArticles(true)
+        if (data.articles.length === 0) setNoArticles(true);
       })
       .catch((err) => alert(err))
-      .finally(()=>SetLoading(false))
+      .finally(() => SetLoading(false));
   };
 
-  useEffect(()=>{
-    if(!searchValue?.trim()) setNoArticles(false);
-  },[searchValue])
-
+  useEffect(() => {
+    if (!searchValue?.trim()) setNoArticles(false);
+  }, [searchValue]);
 
   return (
-    <main className="bg-slate-100 flex items-center justify-center w-full h-screen">
-      <div className="items-center justify-center flex flex-col h-full w-full py-12 px-4">
+    <main className="main-container">
+      <div className="wrapper">
         {/* search */}
-        <form
-          onSubmit={(e) => handleSubmit(e)}
-          className="md:w-1/3 w-full mb-12 items-center justify-center flex flex-col space-y-12 "
-        >
+        <form onSubmit={(e) => handleSubmit(e)} className="form-wrapper">
           <input
             type="text"
-            className="w-full rounded-full py-3 px-6 bg-white shadow-md shadow-slate-200"
+            className="search-input"
             placeholder="search for news ..."
             value={searchValue}
             onChange={(e) => SetSearchValue(e.target.value)}
@@ -50,17 +46,21 @@ function App() {
         </form>
         {/* list */}
         {loading ? (
-          <div className="w-full h-full items-center justify-center flex">
-            <h1 className="w-full text-center text-lg"> loading... </h1>
+          <div className="loading-wrapper">
+            <h1 className="loading-text"> loading... </h1>
           </div>
         ) : (
-          <div className="px-12 overflow-y-auto items-start justify-start flex flex-col w-full">
-            <div className="overflow-y-scroll items-start justify-start flex flex-col">
-              {searchResults.map((result,i) => (
+          <div className="body-container">
+            <div className="results-wrapper">
+              {searchResults.map((result, i) => (
                 <ResultItem data={result} key={result.source.id + i} />
-                ))}
-                </div>
-              {noArticles && <h1 className="text-xl w-full text-center"> no articles available for {searchValue}</h1>}
+              ))}
+            </div>
+            {noArticles && (
+              <h1 className="noArticles-text">
+                no articles available for {searchValue}
+              </h1>
+            )}
           </div>
         )}
       </div>
